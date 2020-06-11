f8599e062e75d62854e5116999bc7602d4eedb38
Tom Savage <tsavage@gmail.com>
2020-06-10 23:53:53 -0400

    update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Le Big Mac', FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Big Kahuna Burger', FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Veggie Burger', FALSE);

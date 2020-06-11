b132d065851549265b61140764d389b46cacbabd
Tom Savage <tsavage@gmail.com>
2020-06-11 01:11:17 -0400

    ormburger

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,24 @@
-// Inside burger.js, import orm.js into burger.js
+const orm = require('../config/orm');
+
+var burger = {
+    selectall: function(cb) {
+      orm.selectall("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    
+  };
 
 
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
-
-
-// Export at the end of the burger.js file.
+module.exports = burger
